500724e92c0e5cb03a1468e123de839733146b00
Shand, Brian <Brian.Shand@fpl.com>
2023-09-26 19:35:08 -0400

    90

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import './Expenses.css'
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import { useState } from 'react';
+import ExpensesList from './ExpensesList';
 
 
 function Expenses(props) {
@@ -10,8 +10,6 @@ function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState('2020')
 
     const filterChangeHandler = selectedYear => {
-        console.log('Expenses.js');
-        console.log(selectedYear);
         setFilteredYear(selectedYear);
     };
 
@@ -19,28 +17,16 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === filteredYear; //creates new list if true/false
     })
 
-    let expensesContent = <p>No expenses found.</p>;
 
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        ))
-    }
 
     return (
         <div>
-
             <Card className="expenses">
                 <ExpensesFilter
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-                {expensesContent}
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     )
